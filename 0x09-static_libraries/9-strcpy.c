891ef4f3d7284762014b80bc3bd46f45134727f5
Brunokayitare <b.kayitare@alustudent.com>
2023-09-29 15:29:03 -0700

    updated

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -10,12 +9,11 @@ char *_strcpy(char *dest, char *src)
 {
 int l = 0;
 int x = 0;
-
 while (*(src + l) != '\0')
 {
 l++;
 }
-for (; x < l; x++)
+for ( ; x < l ; x++)
 {
 dest[x] = src[x];
 }
